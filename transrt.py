b88559fd9e52418ff9db3fe018d0ac3972f688b8
cryptokatra <129519216+cryptokatra@users.noreply.github.com>
2023-04-24 22:07:09 +0800

    Update transrt.py

--- transrt.py
+++ transrt.py
@@ -1,53 +1,81 @@
-import streamlit as st 
-from easynmt import EasyNMT 
- 
-model = EasyNMT('opus-mt') 
-LANGS = ['ja', 'es', 'fr', 'en', 'ko'] 
-LANG_NAMES = {'ja': '日本語', 'es': 'スペイン語', 'fr': 'フランス語', 'en': '英語', 'ko': '韓国語'} 
- 
-def translate(sent, source, target): 
-    return model.translate(sent, source_lang=source, target_lang=target) 
- 
-def display_props(): 
-    st.markdown("## Subtitle Translator") 
-    st.sidebar.markdown('## Possible Translations') 
-    for lang in LANGS: 
-        st.sidebar.markdown("* English _(en)_ - {} _({})_".format(LANG_NAMES[lang], lang)) 
-    st.sidebar.text("\n") 
-    st.sidebar.text("\n") 
-    st.sidebar.markdown('Powered by: [EasyNMT](https://pypi.org/project/EasyNMT/0.0.7/) and [Streamlit](https://www.streamlit.io/)') 
-    return 
- 
-def save_file(filename, content): 
-    with open(filename, "w", encoding="utf-8-sig") as f: 
-        f.write(content) 
- 
-display_props() 
- 
-source_lang_content = st.file_uploader("Upload SRT file", type=['srt']) 
- 
-if source_lang_content is not None: 
-    all_data = [line.decode().strip() for line in source_lang_content.readlines()] 
-    num_lines = len(all_data) 
-    step = int(num_lines/100) 
- 
-    for lang in LANGS: 
-        st.markdown('Processing from English to {}'.format(LANG_NAMES[lang])) 
-        big_srt_text = "" 
-        outfile = open('{}_translated.srt'.format(lang), 'w', encoding="utf-8-sig") 
-        v = 0 
-        for idx, line in enumerate(all_data): 
-            if v!=100: 
-                if idx%step==0: 
-                    v+=1 
-            if line and line[0].isdigit(): 
-                big_srt_text += line+'\n' 
-            elif line.strip(): 
-                converted_line = translate(line, 'en', lang) 
-                big_srt_text += converted_line+'\n' 
-            else: 
-                big_srt_text += '\n' 
- 
-        save_file('{}_translated.srt'.format(lang), big_srt_text) 
-    st.markdown('### Translations Done!!') 
-    st.write('Files saved on Disk')
+import streamlit as st
+from easynmt import EasyNMT
+import chardet
+import opencc
+
+model = EasyNMT('opus-mt')
+converter = opencc.OpenCC('s2twp.json')
+
+LANGS = [('zh', 'ja'), ('zh', 'es'), ('zh', 'fr'), ('zh', 'en'), ('zh', 'ko')]
+
+def translate(sent, source, target):
+    return model.translate(sent, source_lang=source, target_lang=target)
+
+def convert(text):
+    return converter.convert(text)
+
+def detect_encoding(file):
+    rawdata = file.read()
+    return chardet.detect(rawdata)['encoding']
+
+def display_props():
+    st.markdown("## Subtitle Translator")
+    st.sidebar.markdown('## Possible Translations')
+    st.sidebar.text("\n")
+    st.sidebar.markdown("* Chinese _(zh)_ - Japanese _(ja)_")
+    st.sidebar.markdown("* Chinese _(zh)_ - Spanish _(es)_")
+    st.sidebar.markdown("* Chinese _(zh)_ - French _(fr)_")
+    st.sidebar.markdown("* Chinese _(zh)_ - English _(en)_")
+    st.sidebar.markdown("* Chinese _(zh)_- Korean _(ko)_")
+    st.sidebar.text("\n")
+    st.sidebar.text("\n")
+    st.sidebar.markdown('Powered by: [EasyNMT](https://pypi.org/project/EasyNMT/0.0.7/) and [Streamlit](https://www.streamlit.io/)')
+    return
+
+display_props()
+
+source_lang_content = st.file_uploader("", type=['srt'])
+
+if source_lang_content is not None:
+    all_data = [convert(line.decode(detect_encoding(source_lang_content))) for line in source_lang_content]
+    num_lines = sum(1 for line in all_data)
+    step = int(num_lines/100)
+
+    for idx1, lang_pair in enumerate(LANGS):
+        if idx1 == 0:
+            st.markdown('Processing  __{0}__  to  __{1}__'.format(lang_pair[0], lang_pair[1]))
+            outfile = open('{0}_{1}.srt'.format(lang_pair[0], lang_pair[1]), 'w', encoding='utf-8')
+        if idx1 == 1:
+            st.markdown('Processing  __{0}__  to  __{1}__'.format(lang_pair[0], lang_pair[1]))
+            outfile = open('{0}_{1}.srt'.format(lang_pair[0], lang_pair[1]), 'w', encoding='utf-8')
+        if idx1 == 2:
+            st.markdown('Processing  __{0}__  to  __{1}__'.format(lang_pair[0], lang_pair[1]))
+            outfile = open('{0}_{1}.srt'.format(lang_pair[0], lang_pair[1]), 'w', encoding='utf-8')
+        if idx1 == 3:
+            st.markdown('Processing  __{0}__  to  __{1}__'.format(lang_pair[0], lang_pair[1]))
+            outfile = open('{0}_{1}.srt'.format(lang_pair[0], lang_pair[1]), 'w', encoding='utf-8')
+        if idx1 == 4:
+            st.markdown('Processing  __{0}__  to  __{1}__'.format(lang_pair[0], lang_pair[1]))
+            outfile = open('{0}_{1}.srt'.format(lang_pair[0], lang_pair[1]), 'w', encoding='utf-8')
+
+        my_bar = st.progress(0)
+        big_srt_text = ""
+        v = 0
+        for idx2, line in enumerate(all_data):
+            print(idx2, v, num_lines, step)
+            line = str(line)
+            if v != 100:
+                if idx2 % step == 0:
+                    v += 1
+                    my_bar.progress(v)
+
+            if line[0].isnumeric():
+                big_srt_text += line
+            else:
+                converted_line = translate(line, lang_pair[0], lang_pair[1])
+                big_srt_text += converted_line
+
+        outfile.write(big_srt_text)
+        if idx1 == len(LANGS)-1:
+            st.markdown('### Translations Done!!')
+            st.write('Files saved on Disk')
