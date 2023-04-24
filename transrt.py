80e8c2cc645f9a12452ef2fd3a814d88aa5b7444
cryptokatra <129519216+cryptokatra@users.noreply.github.com>
2023-04-24 21:46:36 +0800

    Update transrt.py

--- transrt.py
+++ transrt.py
@@ -1,46 +1,41 @@
-import streamlit as st
 import os
+import streamlit as st
 from googletrans import Translator
-from typing import List
+from pathlib import Path
 
-def translate_srt_file(srt_file_path: str, language_codes: List[str]):
-    # 打开原始srt文件
-    with open(srt_file_path, 'r', encoding='utf-8') as f:
-        # 读取srt字幕内容
-        srt_content = f.read()
 
-    # 循环翻译每个目标语言
-    for language_code in language_codes:
-        # 创建Google翻译器实例
+def translate_srt_file(srt_file_path, language_codes):
+    try:
         translator = Translator()
-        # 翻译字幕内容
-        translated_content = translator.translate(srt_content, dest=language_code).text
-        # 构造新的文件路径
-        new_file_path = srt_file_path[:-4] + '_' + language_code + '.srt'
+        with open(srt_file_path, 'r', encoding='utf-8') as f:
+            original_srt_text = f.read()
+            for language_code in language_codes:
+                translated_text = translator.translate(original_srt_text, dest=language_code).text
+                output_file_path = os.path.splitext(srt_file_path)[0] + f'_{language_code}.srt'
+                Path(output_file_path).write_text(translated_text)
+    except Exception as e:
+        st.error(str(e))
 
-        # 将翻译后的字幕内容写入新文件
-        with open(new_file_path, 'w', encoding='utf-8') as f:
-            f.write(translated_content)
 
-    st.write("五个srt文件已创建。")
+def main():
+    st.set_page_config(page_title="SRT Translator")
 
-# Streamlit App
-def app():
-    st.title("SRT字幕翻译")
-    st.write("使用Google Translate将SRT字幕翻译成五种目标语言。")
+    st.title("SRT Translator")
 
-    # 显示选择文件按钮
-    srt_file = st.file_uploader("选择一个SRT字幕文件", type="srt")
+    # Create browse file button
+    srt_file = st.file_uploader("Upload your SRT file", type=['srt'])
 
-    if srt_file and st.button("翻译并保存"):
-        # 将checkbox的值作为参数传递给translate_srt_file函数
-        language_codes = [lang_code for lang_code in ["日文", "西班牙文", "法文", "英文", "韩文"] if lang_code in checkbox_state]
-        # 执行翻译并保存
-        translate_srt_file(srt_file.name, language_codes)
+    # Create language checkboxes
+    st.subheader("Choose target languages:")
+    language_codes = ['ja', 'es', 'fr', 'en', 'ko']
+    selected_languages = st.multiselect('Select languages', language_codes, default=language_codes)
 
-# 显示checkbox
-checkbox_state = st.sidebar.multiselect("选择要翻译成的语言", options=["日文", "西班牙文", "法文", "英文", "韩文"], default=["日文", "西班牙文", "法文", "英文", "韩文"])
+    # Create translate button
+    if st.button("Translate and save"):
+        if srt_file is not None:
+            srt_file_path = srt_file.name
+            translate_srt_file(srt_file_path, selected_languages)
+            st.success("Translation completed and files saved to local directory!")
 
-# 运行Streamlit App
-if __name__ == '__main__':
-    app()
+if __name__ == "__main__":
+    main()
