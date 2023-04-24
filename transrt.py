7dcd9f17c67969d45ee5e64b9cf09ae89ef1581c
cryptokatra <129519216+cryptokatra@users.noreply.github.com>
2023-04-24 22:16:34 +0800

    Update transrt.py

--- transrt.py
+++ transrt.py
@@ -1,81 +1,44 @@
 import streamlit as st
 from easynmt import EasyNMT
-import chardet
-import opencc
+from pathlib import Path
+from pysubs2 import SSAFile,SSAEvent
 
 model = EasyNMT('opus-mt')
-converter = opencc.OpenCC('s2twp.json')
 
-LANGS = [('zh', 'ja'), ('zh', 'es'), ('zh', 'fr'), ('zh', 'en'), ('zh', 'ko')]
+TARGET_LANGS = ['ja', 'es', 'fr', 'en', 'ko']
 
 def translate(sent, source, target):
     return model.translate(sent, source_lang=source, target_lang=target)
 
-def convert(text):
-    return converter.convert(text)
+st.sidebar.markdown("## 字幕翻译器")
+st.sidebar.text("\n")
+st.sidebar.markdown("目标语言：日语、西班牙语、法语、英语、韩语")
+st.sidebar.text("\n")
+st.sidebar.text("\n")
+st.sidebar.markdown('Powered by EasyNMT and Streamlit')
 
-def detect_encoding(file):
-    rawdata = file.read()
-    return chardet.detect(rawdata)['encoding']
+input_subtitle = st.file_uploader("请上传SRT字幕文件", type='srt')
 
-def display_props():
-    st.markdown("## Subtitle Translator")
-    st.sidebar.markdown('## Possible Translations')
-    st.sidebar.text("\n")
-    st.sidebar.markdown("* Chinese _(zh)_ - Japanese _(ja)_")
-    st.sidebar.markdown("* Chinese _(zh)_ - Spanish _(es)_")
-    st.sidebar.markdown("* Chinese _(zh)_ - French _(fr)_")
-    st.sidebar.markdown("* Chinese _(zh)_ - English _(en)_")
-    st.sidebar.markdown("* Chinese _(zh)_- Korean _(ko)_")
-    st.sidebar.text("\n")
-    st.sidebar.text("\n")
-    st.sidebar.markdown('Powered by: [EasyNMT](https://pypi.org/project/EasyNMT/0.0.7/) and [Streamlit](https://www.streamlit.io/)')
-    return
+if input_subtitle is not None:
+    input_content = SSAFile.from_string(input_subtitle.read().decode('utf-8'))
+    st.write('上传成功！')
 
-display_props()
+    translated_subtitles = {}
+    for lang in TARGET_LANGS:
+        st.write(f'开始翻译为{lang}...')
+        translated_subs = SSAFile()
+        for event in input_content.events:
+            translated_text = translate(event.text, 'zh', lang)
+            translated_subs.events.append(
+                SSAEvent(start=event.start,
+                         end=event.end,
+                         text=translated_text))
+        translated_subtitles[lang] = translated_subs
+        st.write(f'{lang}翻译完成！')
 
-source_lang_content = st.file_uploader("", type=['srt'])
-
-if source_lang_content is not None:
-    all_data = [convert(line.decode(detect_encoding(source_lang_content))) for line in source_lang_content]
-    num_lines = sum(1 for line in all_data)
-    step = int(num_lines/100)
-
-    for idx1, lang_pair in enumerate(LANGS):
-        if idx1 == 0:
-            st.markdown('Processing  __{0}__  to  __{1}__'.format(lang_pair[0], lang_pair[1]))
-            outfile = open('{0}_{1}.srt'.format(lang_pair[0], lang_pair[1]), 'w', encoding='utf-8')
-        if idx1 == 1:
-            st.markdown('Processing  __{0}__  to  __{1}__'.format(lang_pair[0], lang_pair[1]))
-            outfile = open('{0}_{1}.srt'.format(lang_pair[0], lang_pair[1]), 'w', encoding='utf-8')
-        if idx1 == 2:
-            st.markdown('Processing  __{0}__  to  __{1}__'.format(lang_pair[0], lang_pair[1]))
-            outfile = open('{0}_{1}.srt'.format(lang_pair[0], lang_pair[1]), 'w', encoding='utf-8')
-        if idx1 == 3:
-            st.markdown('Processing  __{0}__  to  __{1}__'.format(lang_pair[0], lang_pair[1]))
-            outfile = open('{0}_{1}.srt'.format(lang_pair[0], lang_pair[1]), 'w', encoding='utf-8')
-        if idx1 == 4:
-            st.markdown('Processing  __{0}__  to  __{1}__'.format(lang_pair[0], lang_pair[1]))
-            outfile = open('{0}_{1}.srt'.format(lang_pair[0], lang_pair[1]), 'w', encoding='utf-8')
-
-        my_bar = st.progress(0)
-        big_srt_text = ""
-        v = 0
-        for idx2, line in enumerate(all_data):
-            print(idx2, v, num_lines, step)
-            line = str(line)
-            if v != 100:
-                if idx2 % step == 0:
-                    v += 1
-                    my_bar.progress(v)
-
-            if line[0].isnumeric():
-                big_srt_text += line
-            else:
-                converted_line = translate(line, lang_pair[0], lang_pair[1])
-                big_srt_text += converted_line
-
-        outfile.write(big_srt_text)
-        if idx1 == len(LANGS)-1:
-            st.markdown('### Translations Done!!')
-            st.write('Files saved on Disk')
+    if st.button("下载翻译后的字幕文件"):
+        for lang in TARGET_LANGS:
+            output_file_name = f"{Path(input_subtitle.name).stem}-{lang}.srt"
+            with open(output_file_name, 'w') as f:
+                f.write(translated_subtitles[lang].to_string())
+                st.markdown(f'[点此下载 {output_file_name}]({output_file_name})')
